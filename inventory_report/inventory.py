ffcab1de43767e0a431a2f8f11af40ef3032e947
alanguilhermeM <alanguilherme13@outlook.com>
2023-12-03 18:26:46 -0300

    Finalizado requisito 5

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from inventory_report.product import Product
+from typing import List, Optional
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None) -> None:
+        self._data = data if data is not None else []
+
+    @property
+    def data(self):
+        return self._data.copy()
+
+    def add_data(self, data: List[Product]) -> None:
+        self._data.extend(data)
