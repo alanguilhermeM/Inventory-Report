49227b8a7f78e6fff2e92940023ba6d14c768691
alanguilhermeM <alanguilherme13@outlook.com>
2023-12-03 16:55:33 -0300

    Finalizado requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,16 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    instance = Product(
+        "2", "book", "livraria", "20/04/2001", "20/04/2003", "DAF8DJ2", "non"
+    )
+
+    expected_output = (
+        "The product 2 - book with serial number DAF8DJ2 manufactured on "
+        "20/04/2001 by the company livraria valid until 20/04/2003 must be "
+        "stored according to the following instructions: non."
+    )
+    assert instance.__str__() == expected_output
+
+
